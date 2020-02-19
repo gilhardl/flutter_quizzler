2fb6c59d98d0fc785d08b9d88d3b251867b1efe2
Lucas GILHARD <l.gilhard@gmail.com>
2020-02-19 13:32:14 +0100

    add score keeper UI

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,8 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -61,7 +63,14 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked true.
+                setState(() {
+                  scoreKeeper.add(
+                    Icon(
+                      Icons.check,
+                      color: Colors.green,
+                    ),
+                  );
+                });
               },
             ),
           ),
@@ -84,7 +93,12 @@ class _QuizPageState extends State<QuizPage> {
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        SizedBox(
+          height: 24.0,
+          child: Row(
+            children: scoreKeeper,
+          ),
+        )
       ],
     );
   }
