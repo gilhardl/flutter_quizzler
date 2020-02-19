830103547519cbc697cfe48a26edfd4e0a81456f
Lucas GILHARD <l.gilhard@gmail.com>
2020-02-19 14:58:05 +0100

    display questions

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,12 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.'
+  ];
+  int questionNumber = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -39,7 +45,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -64,12 +70,10 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 setState(() {
-                  scoreKeeper.add(
-                    Icon(
-                      Icons.check,
-                      color: Colors.green,
-                    ),
-                  );
+                  questionNumber++;
+                  if (questionNumber >= questions.length) {
+                    questionNumber = 0;
+                  }
                 });
               },
             ),
@@ -88,7 +92,12 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+                setState(() {
+                  questionNumber++;
+                  if (questionNumber >= questions.length) {
+                    questionNumber = 0;
+                  }
+                });
               },
             ),
           ),
